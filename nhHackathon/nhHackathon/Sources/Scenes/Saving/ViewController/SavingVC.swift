6a6af3813465a6af2d8823bf1c302e41ec399f3b
jaeeunahn <jaeeunno1@naver.com>
2020-12-12 15:45:16 +0900

    [CREATE] saving view layout fin

--- nhHackathon/nhHackathon/Sources/Scenes/Saving/ViewController/SavingVC.swift
+++ nhHackathon/nhHackathon/Sources/Scenes/Saving/ViewController/SavingVC.swift
@@ -9,21 +9,160 @@ import UIKit
 
 class SavingVC: UIViewController {
 
+    //MARK: - Init
+    
+    
+    @IBOutlet weak var imageView: UIView! {
+        didSet {
+            self.imageView.roundTopCorners(cornerRadius: 62)
+        }
+    }
+    @IBOutlet weak var saveAmountLabel: UILabel!
+    
+    @IBOutlet weak var imageViewConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var textView: UIView! {
+        didSet {
+            self.textView.backgroundColor = .white
+            self.textView.setBorder(borderColor: .veryLightPink, borderWidth: 1)
+            self.textView.makeRounded(cornerRadius: 3)
+        }
+    }
+    
+    @IBOutlet weak var textField: UITextField! {
+        didSet {
+            self.textField.tintColor = .salmon
+            self.textField.delegate = self
+        }
+    }
+    
+    @IBOutlet weak var saveButton: UIButton! {
+        didSet {
+            self.saveButton.makeRounded(cornerRadius: 22.5)
+            self.saveButton.backgroundColor = .salmon
+            self.saveButton.dropShadow(color: .black, offSet: CGSize(width: 0, height: 3), opacity: 0.16, radius: 6)
+        }
+    }
+    
+    
+    //MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        initGestureRecognizer()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        unregisterForKeyboardNotifications()
+    }
+    
+    //MARK: - Action
+    
+    func typingStarted() {
+        self.textView.setBorder(borderColor: .salmon, borderWidth: 1)
+    }
+    
+    func noTyping() {
+        self.textView.setBorder(borderColor: .veryLightPink, borderWidth: 1)
+    }
+    
+    
+    @IBAction func back(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension SavingVC: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        registerForKeyboardNotifications()
+        typingStarted()
     }
-    */
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if string != "" {
+            typingStarted()
+        } else {
+            noTyping()
+        }
+        return true
+    }
+}
 
+
+extension SavingVC: UIGestureRecognizerDelegate {
+    
+    func initGestureRecognizer() {
+        let textFieldTap = UITapGestureRecognizer(target: self, action: #selector(handleTapTextField(_:)))
+        textFieldTap.delegate = self
+        view.addGestureRecognizer(textFieldTap)
+    }
+    
+    // 다른 위치 탭했을 때 키보드 없어지는 코드
+    @objc func handleTapTextField(_ sender: UITapGestureRecognizer) {
+        self.textField.resignFirstResponder()
+    }
+    
+    
+    func gestureRecognizer(_ gestrueRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        if (touch.view?.isDescendant(of: textField))! {
+            return false
+        }
+        return true
+    }
+    
+    // keyboard가 보여질 때 어떤 동작을 수행
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        
+        guard let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+        guard let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else { return }
+        
+        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardHeight: CGFloat
+        
+        if #available(iOS 11.0, *) {
+            keyboardHeight = keyboardFrame.cgRectValue.height - self.view.safeAreaInsets.bottom
+        } else {
+            keyboardHeight = keyboardFrame.cgRectValue.height
+        }
+        
+        // animation
+        UIView.animate(withDuration: duration, delay: 0.0, options: .init(rawValue: curve), animations: {
+            
+            // +로 갈수록 y값이 내려가고 -로 갈수록 y값이 올라간다.
+            self.imageViewConstraint.constant = 233
+            //121
+            
+        })
+        
+        self.view.layoutIfNeeded()
+    }
+    
+    // keyboard가 사라질 때 어떤 동작을 수행
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        guard let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else {return}
+        guard let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else {return}
+        UIView.animate(withDuration: duration, delay: 0.0, options: .init(rawValue: curve), animations: {
+            
+            self.imageViewConstraint.constant = 0
+            
+        })
+        
+        self.view.layoutIfNeeded()
+    }
+    
+    
+    // observer
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unregisterForKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
 }
